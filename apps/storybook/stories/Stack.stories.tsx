8ec98eb115d492af8ce381518d8214cd67212622
Roberto Ríos Perea <roberto.rios.pere@outlook.com>
2022-07-05 23:28:50 +0200

    feat(stack): add stack component

---
+++ apps/storybook/stories/Stack.stories.tsx
@@ -0,0 +1,100 @@
+import Stack from '@rrios-dev/react-stack';
+export default {
+  title: 'Example/Stack',
+  component: Stack,
+  argTypes: {
+    direction: {
+      control: 'select',
+      options: ['column', 'row'],
+      defaultValue: undefined,
+    },
+    justifyContent: {
+      control: 'select',
+      options: ['center', 'start', 'end'],
+      defaultValue: undefined,
+    },
+    alignItems: {
+      control: 'select',
+      options: ['center', 'start', 'end'],
+      defaultValue: undefined,
+    },
+    spacing: {
+      control: 'select',
+      options: [1, 2, 3, 4, 5, 6, 7],
+      defaultValue: 1,
+    },
+    fullWidth: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+    fullHeight: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+    count: {
+      control: 'number',
+      defaultValue: 3,
+    },
+  },
+};
+
+export const Main = ({ count, ...args }) => (
+  <Stack
+    {...args}
+    css={{
+      '& > div': {
+        backgroundColor: '$foreground',
+        color: '$text',
+        borderRadius: '$1',
+        p: 1,
+      },
+    }}
+  >
+    {new Array(...new Array(count).keys()).map((idx) => (
+      <div key={idx}>Item {idx + 1}</div>
+    ))}
+  </Stack>
+);
+
+export const FullWidth = () => {
+  return (
+    <Stack
+      direction="column"
+      fullWidth
+      spacing={1}
+      css={{
+        '& > div': {
+          backgroundColor: '$foreground-dark-0',
+          color: '$text',
+          borderRadius: '$1',
+          p: 1,
+        },
+      }}
+    >
+      {new Array(...new Array(8).keys()).map((idx) => (
+        <div key={idx}>Item {idx + 1}</div>
+      ))}
+    </Stack>
+  );
+};
+
+export const DirectionColumn = () => {
+  return (
+    <Stack
+      direction="column"
+      spacing={1}
+      css={{
+        '& > div': {
+          backgroundColor: '$foreground-dark-0',
+          color: '$text',
+          borderRadius: '$1',
+          p: 1,
+        },
+      }}
+    >
+      {new Array(...new Array(8).keys()).map((idx) => (
+        <div key={idx}>Item {idx + 1}</div>
+      ))}
+    </Stack>
+  );
+};
---
+++ packages/@rrios-dev__react-stack/src/Stack.ts
@@ -0,0 +1,75 @@
+import { styled } from '@rrios-dev/styled';
+
+const Stack = styled('div', {
+  display: 'flex',
+  variants: {
+    fullWidth: {
+      true: {
+        width: '100%',
+      },
+      false: {
+        width: 'fit-content',
+      },
+    },
+    fullHeight: {
+      true: {
+        height: '100%',
+      },
+    },
+    justifyContent: {
+      center: {
+        justifyContent: 'center',
+      },
+      start: {
+        justifyContent: 'flex-start',
+      },
+      end: {
+        justifyContent: 'flex-end',
+      },
+    },
+    alignItems: {
+      center: {
+        alignItems: 'center',
+      },
+      start: {
+        alignItems: 'flex-start',
+      },
+      end: {
+        alignItems: 'flex-end',
+      },
+    },
+    spacing: {
+      1: {
+        gap: 1,
+      },
+      2: {
+        gap: 2,
+      },
+      3: {
+        gap: 3,
+      },
+      4: {
+        gap: 4,
+      },
+      5: {
+        gap: 5,
+      },
+      6: {
+        gap: 6,
+      },
+      7: {
+        gap: 7,
+      },
+    },
+    direction: {
+      column: {
+        flexDirection: 'column',
+      },
+      row: {
+        flexDirection: 'row',
+      },
+    },
+  },
+});
+
+export default Stack;
