2c9bd2a2f9652d77e632878f245619c0dab84adf
Roberto Ríos Perea <roberto.rios.pere@outlook.com>
2022-08-04 23:58:20 +0200

    fix: include templates on root templates-cli package

--- packages/templates-cli/CHANGELOG.md
+++ packages/templates-cli/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @rrios-dev/templates-cli
 
+## 0.0.5
+
+### Patch Changes
+
+- include templates into root package
+
 ## 0.0.4
 
 ### Patch Changes
--- packages/templates-cli/scripts/copyTemplates.js
+++
@@ -1,8 +0,0 @@
-const { cpSync } = require('fs');
-const { resolve } = require('path');
-
-const DIR_ROOT = resolve(__dirname, '..');
-const DIR_TEMPLATES = resolve(DIR_ROOT, 'templates');
-const DIR_BUILD = resolve(DIR_ROOT, 'dist', 'templates');
-
-cpSync(DIR_TEMPLATES, DIR_BUILD, { recursive: true });
